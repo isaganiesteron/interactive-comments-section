487de4b5d1718eb0814223e453951e5aef486a4e
Isagani Esteron <isaganiesteron@ymail.com>
2023-11-03 22:52:13 +0800

    finished comment and subcomment components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { Typography, Grid, CssBaseline, Box, Avatar, Button, ButtonGroup } from "@mui/material"
 import { useEffect, useState } from "react"
 import Comment from "./components/Comment"
+import Subcomment from "./components/Subcomment"
+import { createTheme, ThemeProvider } from "@mui/material"
+
+const theme = createTheme({
+	palette: {
+		background: { default: "#f5f6fa" },
+	},
+})
 
 function App() {
 	const [data, setData] = useState()
@@ -23,18 +31,19 @@ function App() {
 	}, [])
 
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<Grid container direction="row" sx={{ marginTop: "50px;" }}>
 				<Grid xs={3} itemType=""></Grid>
-				<Grid xs={6} container item direction="column" justifyContent="center">
+				<Grid xs={6} container item direction="column" justifyContent="center" alignContent={"center"}>
 					<Comment />
+					<Subcomment />
 				</Grid>
 				<Grid xs={3} item></Grid>
 				{/* <Typography variant="h1">Interactive Comments Section</Typography>
 			{data ? <Typography variant="body1">{JSON.stringify(data)}</Typography> : <Typography>No Data</Typography>} */}
 			</Grid>
-		</>
+		</ThemeProvider>
 	)
 }
 
--- src/components/Subcomment.jsx
+++ src/components/Subcomment.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { Typography, Grid, Avatar, Button, ButtonGroup } from "@mui/material"
 
-const Comment = () => {
+const Subcomment = () => {
 	return (
-		<Grid container item direction="row" columnSpacing={2} justifyContent={"flex-start"} sx={{ p: "20px 20px 20px 0", m: "5px", borderRadius: "10px", backgroundColor: "white", width: "600px" }}>
+		<Grid container item direction="row" columnSpacing={2} justifyContent={"flex-start"} sx={{ p: "20px 20px 20px 0", marginLeft: "70px", borderRadius: "10px", backgroundColor: "white", width: "535px" }}>
 			<Grid container item xs={1} justifyContent={"center"} sx={{ paddingRight: "20px" }}>
 				<ButtonGroup variant="outlined" orientation="vertical" disableElevation sx={{ border: "0px", width: "20px", borderRadius: "20px" }}>
 					<Button sx={{ border: "0px", backgroundColor: "#faf9fe", "&:hover": { border: "0" } }}>+</Button>
@@ -25,11 +25,15 @@ const Comment = () => {
 					</Grid>
 				</Grid>
 				<Grid item container direction="column" justifyContent={"flex-start"} sx={{ paddingTop: "10px" }}>
-					<Typography variant="body2">Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsivness at various breakpoints works really well. </Typography>
+					<Typography variant="body2">
+						Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsivness at various breakpoints works really well. Impressive! Though it seems the drag feature could be improved. But overall it looks
+						incredible. You've nailed the design and the responsivness at various breakpoints works really well.Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsivness at various breakpoints works
+						really well.
+					</Typography>
 				</Grid>
 			</Grid>
 		</Grid>
 	)
 }
 
-export default Comment
+export default Subcomment
